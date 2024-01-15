f4e2cbcf11e2c87da0d114df8164816c3b93934e
dieulinh <linhvn09@gmail.com>
2024-01-15 18:07:57 -0500

    product model and init vite project for frontend

--- products/models.py
+++ products/models.py
@@ -1,9 +1,29 @@
 from django.db import models
-
+from django.utils import timezone
+from django.conf import settings
 # Create your models here.
 class Product(models.Model):
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1,on_delete=models.CASCADE)
   name = models.CharField(max_length=255)
-  price = models.FloatField()
+  handle = models.SlugField(unique=True)
+  price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99)
+  stripe_price = models.IntegerField(default=999)
+  og_price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99)
   stock = models.IntegerField()
-  image = models.CharField(max_length=2083)
+  image_url = models.CharField(max_length=2083)
+  timestamp = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+  def save(self, *args,**kwargs):
+    if self.price != self.og_price:
+      self.og_price = self.price
+      self.stripe_price = int(self.price*100)
+    super().save(*args, **kwargs)  
+
+class Offer(models.Model):
+  code = models.CharField(max_length=10)
+  description = models.CharField(max_length=255)
+  discount = models.FloatField()
+class Cart(models.Model):
+  created_at = models.DateTimeField()
+  # product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
 
--- products/views.py
+++ products/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Product
 # Create your views here.
 def home(request):
   return HttpResponse('Hello')
+class ProductList(ListView):
+  model = Product
+  template_name = 'products/product_list.html'
